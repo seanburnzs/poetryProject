595f4eba7eece2493926549a9f8d1185dba96939
Sean Burns <seanburnzss@gmail.com>
2025-01-09 05:34:12 -0600

    update settings for s3

--- poetry_project/settings.py
+++ poetry_project/settings.py
@@ -151,8 +151,12 @@ AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_DEFAULT_ACL = 'public-read'
+
+# S3 for media storage
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Serve media from S3
+MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
@@ -196,7 +200,6 @@ STATICFILES_FINDERS = [
 ]
 
 # Media files configuration
-MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
