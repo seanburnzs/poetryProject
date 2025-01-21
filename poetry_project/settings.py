8b54098e42b1baec77ef0a3c0402922bdb618973
Sean Burns <seanburnzss@gmail.com>
2025-01-20 18:41:50 -0600

    update settings for s3

--- poetry_project/settings.py
+++ poetry_project/settings.py
@@ -152,6 +152,14 @@ AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME')
 
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+    'ACL': 'public-read',
+}
+
+AWS_QUERYSTRING_AUTH = False
+
 # S3 for media storage
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
