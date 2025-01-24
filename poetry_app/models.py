da1ea25771d38ed524cd62cc9b1dc64398e4f1c0
Sean Burns <seanburnzss@gmail.com>
2025-01-24 14:53:42 -0600

    add updated at in the profile model

--- poetry_app/models.py
+++ poetry_app/models.py
@@ -103,6 +103,7 @@ class Profile(models.Model):
     streak = models.IntegerField(default=0)
     last_active = models.DateField(null=True, blank=True)
     badges = models.ManyToManyField(Badge, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
