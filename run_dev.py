2c4179fe862bcaeac479f418519d39059428687f
Sean Burns <seanburnzss@gmail.com>
2025-02-11 20:16:31 -0600

    remove dev run file

--- run_dev.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-# development settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poetry_project.settings_dev')
-
-print("STARTING DEV SERVER...")
-subprocess.run([os.path.join(os.environ['VIRTUAL_ENV'], 'Scripts', 'python'), 'manage.py', 'runserver', '8080'])
